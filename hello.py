e3cfa7bcfb07604a39c8a8ad570aef2684b43680
Hafiz Patwary <hafiz.patwary@ericsson.com>
2021-02-01 13:07:09 +0000

    running ansible to deploy lambda

--- hello.py
+++ hello.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Update Lambda! Deployed with Github Actions')
+        'body': json.dumps('Hello from Update Lambda! Deployed with Ansible Actions')
     }
