4d903aefba086a1f441b86eccf8dc67f3efa0a90
Miran Muthanna <43168423+miran93@users.noreply.github.com>
2023-05-08 14:46:00 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Update Lambda! Deployed with Ansible Actions')
+        'body': json.dumps('Hello from Miran!')
     }
